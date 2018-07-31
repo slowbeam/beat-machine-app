64fad3846c470a560059fc59330ddeb82b834ab1
DevinPierce <devinmpierce@gmail.com>
2018-07-31 15:43:16 -0400

    add play/stop functions, volume control

--- beat-machine-frontend/index.js
+++ beat-machine-frontend/index.js
@@ -17,6 +17,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
 })
 
+
+
+
+
 let steps = []
 
 function instantiateSteps() {
@@ -55,27 +59,56 @@ function addNoteToSequence(instrument, stepNum) {
   new Note(loadedDrumKit[instrument][stepNum], instrument, steps[stepNum])
 }
 addNoteToSequence('kick', 0)
-addNoteToSequence('kick', 4)
-addNoteToSequence('kick', 8)
-addNoteToSequence('kick', 12)
+addNoteToSequence('hiHatOpen', 2)
 addNoteToSequence('snare', 4)
+addNoteToSequence('kick', 6)
+addNoteToSequence('kick', 8)
+addNoteToSequence('kick', 11)
 addNoteToSequence('snare', 12)
+addNoteToSequence('kick', 13)
+addNoteToSequence('kick', 13)
+addNoteToSequence('kick', 14)
+addNoteToSequence('kick', 15)
+addNoteToSequence('hiHatClosed', 0)
+addNoteToSequence('hiHatClosed', 7)
+addNoteToSequence('hiHatClosed', 8)
+addNoteToSequence('hiHatClosed', 9)
+addNoteToSequence('hiHatClosed', 10)
+addNoteToSequence('hiHatClosed', 11)
+addNoteToSequence('hiHatClosed', 12)
+addNoteToSequence('hiHatClosed', 13)
+addNoteToSequence('hiHatClosed', 14)
+addNoteToSequence('hiHatClosed', 15)
 
 function removeNoteFromSequence(instrument, stepNum) {
   let targetIndex;
-  steps[stepNum].notes.forEach( (note, i) => {
-    if (note.instrument === instrument) {targetIndex = i};
-  })
-  steps[stepNum].notes.splice(targetIndex, 1) // might delete all notes at step I think?!?
+  let notes = steps[stepNum].notes
+  for (let i = 0; i < notes.length; i++){
+    if (notes[i].instrument === instrument) {targetIndex = i; break;};
+  }
+  notes.splice(targetIndex, 1) // might sometimes delete all notes at step I think?!? maybe not
 }
 // removeNoteFromSequence('kick', 0)
 
-function setTempo(n) {
-  return (60000 / n) / 4
+function changeVolumeOfInstrument(instrument, level) {
+  loadedDrumKit[instrument].forEach(audioEl=>audioEl.volume = level)
 }
 
-stepCount = 1
-// setInterval(playBeats, setTempo(100))
+let playBack;
+let tempo = 100
+
+function setTempo(tempo) {
+  return (60000 / tempo) / 4
+}
+
+function startPlay() {
+  stepCount = 1
+  playBack = setInterval(playBeats, setTempo(tempo))
+}
+
+function stopPlay() {
+  clearInterval(playBack)
+}
 
 function playBeats() {
   console.log(stepCount);
@@ -91,39 +124,3 @@ function playBeats() {
     stepCount++
   }
 }
-
-
-
-// var kick = new Audio('audio/LF_kick_08.wav')
-// let note1 = new Note(kick, steps[0])
-// let note2 = new Note(kick, steps[4])
-// let note3 = new Note(kick, steps[8])
-// let note4 = new Note(kick, steps[12])
-//
-// var snare = new Audio('audio/LF_snare_01.wav')
-// var snare2 = new Audio('audio/LF_snare_01.wav')
-// let note5 = new Note(snare, steps[4])
-// let note6 = new Note(snare2, steps[12])
-//
-// var hiHat = new Audio('audio/LF_hihat_closed_05.wav')
-// var hiHat2 = new Audio('audio/LF_hihat_closed_05.wav')
-// var hiHat3 = new Audio('audio/LF_hihat_closed_05.wav')
-// var hiHat4 = new Audio('audio/LF_hihat_closed_05.wav')
-// var hiHatOpen = new Audio('audio/LF_hihat_open_03.wav')
-// var hiHatOpen2 = new Audio('audio/LF_hihat_open_03.wav')
-// let note7 = new Note(hiHat, steps[0])
-// let note8 = new Note(hiHat2, steps[1])
-// let note9 = new Note(hiHat3, steps[2])
-// let note10 = new Note(hiHat4, steps[3])
-// let note11 = new Note(hiHat, steps[4])
-// let note12 = new Note(hiHat2, steps[5])
-// let note13 = new Note(hiHat3, steps[6])
-// let note14 = new Note(hiHat4, steps[7])
-// let note15 = new Note(hiHat, steps[8])
-// let note16 = new Note(hiHat2, steps[9])
-// let note17 = new Note(hiHatOpen, steps[10])
-// // let note18 = new Note(hiHat4, steps[11])
-// let note19 = new Note(hiHat, steps[12])
-// let note20 = new Note(hiHat2, steps[13])
-// let note21 = new Note(hiHat3, steps[14])
-// let note22 = new Note(hiHat4, steps[15])
