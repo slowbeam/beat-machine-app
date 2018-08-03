c0c19711495aa8afe415bfbd0d79d5d45150e871
Shing Wong <shing2487@gmail.com>
2018-08-03 09:15:12 -0400

    added api-server and updated btn lighting functions

--- beat-machine/app/controllers/api/v1/beats_controller.rb
+++ beat-machine/app/controllers/api/v1/beats_controller.rb
@@ -1,28 +1,27 @@
 class Api::V1::BeatsController < ApplicationController
-  before_action :find_beat, only: [:update, :destroy]
+  before_action :find_beat, only: [:show, :update, :destroy]
 
   def index
     @beats = Beat.all
-    render json: @beats
+    render json: @beats, status: 200
+  end
+
+  def show
+    render json: @beat, status: 200
   end
 
   def create
-    Beat.create(beat_params)
-    redirect_to api_v1_beats_path
+    @beat = Beat.create(beat_params)
+    render json: @beat, status: 201
   end
 
   def update
     @beat.update(beat_params)
-    if @beat.save
-      render json: @beat, status: :accepted
-    else
-      render json: { errors: @beat.errors.full_messages }, status: :unprecessible_entity
-    end
+    render json: @beat, status: 202
   end
 
   def destroy
     @beat.destroy
-    redirect_to api_v1_beats_path
   end
 
   private
--- beat-machine/config/routes.rb
+++ beat-machine/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :beats, only: [:index, :create, :update, :destroy]
+      resources :beats, only: [:index, :show, :create, :update, :destroy]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
